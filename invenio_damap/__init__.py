0268a6d4c6502f1b1fee11dd16dbf7f4fd6dddfb
Sotiris Tsepelakis <stsepelakis@gmail.com>
2024-09-19 13:42:21 +0200

    📦 release: v0.1.0

--- invenio_damap/__init__.py
+++ invenio_damap/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2024 TU Wien.
 #
 # Invenio-DAMAP is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -9,6 +10,6 @@
 
 from .ext import InvenioDAMAP
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 __all__ = ("__version__", "InvenioDAMAP")
